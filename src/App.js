b612b05186dce58184e5c719e2bfaae9f653934b
ml-aakash-dev <ml.aakash.dev@gmail.com>
2021-11-25 00:25:22 +0500

    origin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Blog from './Blog/Blog'
+import { Route, Routes } from 'react-router';
+import AppleAirPodBestWirelessEarBuds from './Blog/apple-airpods-best-wireless-earbuds';
+import WaysToKeepYourSmartPhonesClean from './Blog/ways-to-keep-your-smartphones-clean';
+import AirpodsVsAirpodPro from './Blog/airpods-vs-airpods-pro';
+import MakeYourPhoneWorkFaster from './Blog/make-your-phone-work-faster';
+import Iphone11HeavyDutyDefenderCase from './Blog/iphone-11-heavy-duty-defender-case'
+import ImportanceOfMobilePhoneAccessories from './Blog/importance-of-mobile-phone-accessories'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/'  element={<Blog/>} />
+      <Route path='/blog/apple-airpods-best-wireless-earbuds'  element={<AppleAirPodBestWirelessEarBuds/>} />
+      <Route path='/blog/ways-to-keep-your-smartphones-clean'  element={<WaysToKeepYourSmartPhonesClean/>} />
+      <Route path='/blog/airpods-vs-airpods-pro'  element={<AirpodsVsAirpodPro/>} />
+      <Route path='/blog/make-your-phone-work-faster'  element={<MakeYourPhoneWorkFaster/>} />
+      <Route path='/blog/iphone-11-heavy-duty-defender-case'  element={<Iphone11HeavyDutyDefenderCase/>} />
+      <Route path='/blog/importance-of-mobile-phone-accessories'  element={<ImportanceOfMobilePhoneAccessories/>} />
+        
+
+    </Routes>
   );
 }
 
